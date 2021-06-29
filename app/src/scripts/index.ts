d8a3147feff9b8666278253269b7d0c22c081fc5
Shahriyor <shahriyordeveloping@gmail.com>
2021-06-29 10:03:00 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -2,6 +2,8 @@
 const headerButton: HTMLButtonElement = document.querySelector(
   ".header__button"
 );
+const headerLinkDf: HTMLDivElement = document.querySelector(".header__link__df");
+const galleryMenu: HTMLDivElement = document.querySelector(".gallery__menu");
 const headerMenu: HTMLUListElement = document.querySelector(".header__list");
 let menuOpened = false;
 const menuToggle = () => {
@@ -9,6 +11,14 @@ const menuToggle = () => {
   headerButton.classList.toggle("open");
   headerMenu.classList.toggle("open");
 };
+
+let galleryMenuOpened = false;
+const galleryMenuToggle = () => {
+  galleryMenuOpened = !galleryMenuOpened;
+  galleryMenu.classList.toggle("active");
+}
+
+headerLinkDf.onclick = galleryMenuToggle;
 
 headerButton.onclick = menuToggle;
 
@@ -19,6 +29,14 @@ window.onclick = (e: MouseEvent) => {
     !e.composedPath().includes(headerMenu)
   )
     menuToggle();
+};
+
+window.onclick = (e: MouseEvent) => {
+  if (
+    galleryMenuOpened &&
+    !e.composedPath().includes(headerLinkDf)
+  )
+  galleryMenuToggle();
 };
 
 $('.slider__inner ').slick({
