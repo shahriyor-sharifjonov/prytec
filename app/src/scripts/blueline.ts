845df95dfbfb4d36b3b75f3c2b00f1c319b1aa2a
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-04 19:20:30 +0300

    initial

---
+++ app/src/scripts/blueline.ts
@@ -0,0 +1,10 @@
+let blueline1 = document.querySelector('.blueline-1');
+let blueline2 = document.querySelector('.blueline-2');
+let blueline3 = document.querySelector('.blueline-3');
+let blueline4 = document.querySelector('.blueline-4');
+let blueline5 = document.querySelector('.blueline-5');
+let blueline6 = document.querySelector('.blueline-6');
+
+blueline1.addEventListener('click', ()=>{
+  
+})
